17b6d8049d1d794071f3fbb9c1006e99f6effa80
Marian Eickmeyer <mail@marianheinsen.com>
2025-01-20 08:55:00 +0100

    feat: add http utilities

---
+++ http/index.ts
@@ -0,0 +1,6 @@
+/**
+ * The maximum age of a cookie in seconds.
+ * Chrome caps the expiration date to a maximum of 400 days.
+ * @see https://developer.chrome.com/blog/cookie-max-age-expires
+ */
+export const MAX_COOKIE_AGE = 34560000;
