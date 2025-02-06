79ac32e1cb5db71b1b174c221cf1ccb840f5d30b
Marian Eickmeyer <mail@marianheinsen.com>
2025-02-06 10:20:35 +0100

    feat: make file config optional

--- config/index.ts
+++ config/index.ts
@@ -4,8 +4,20 @@ import { readFileSync } from 'node:fs';
 export function setupConfig<E extends { CONFIG_FILEPATH: string }, F>(
   envSchema: ZodSchema<E>,
   fileSchema: ZodSchema<F>,
-) {
+): { env: E; config: F };
+export function setupConfig<E, F>(envSchema: ZodSchema<E>, fileSchema: ZodSchema<F>): { env: E };
+export function setupConfig<E extends { CONFIG_FILEPATH?: string }, F>(
+  envSchema: ZodSchema<E>,
+  fileSchema: ZodSchema<F>,
+): { env: E; config?: F } {
   const env = envSchema.parse(process.env);
+
+  if (!env.CONFIG_FILEPATH) {
+    return {
+      env,
+    };
+  }
+
   const content = readFileSync(env.CONFIG_FILEPATH, 'utf-8');
   const config = fileSchema.parse(JSON.parse(content));
 
