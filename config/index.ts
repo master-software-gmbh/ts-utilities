eaa1b0f27f3153e23a190588d21c5da35d9e6f03
Marian Eickmeyer <mail@marianheinsen.com>
2025-03-01 10:57:07 +0100

    feat: migrate config to standard schema

--- config/index.ts
+++ config/index.ts
@@ -1,41 +1,44 @@
 import { readFileSync } from 'node:fs';
-import type { ZodSchema, ZodType, z } from 'zod';
 import { logger } from '../logging';
+import type { StandardSchemaV1 } from '@standard-schema/spec';
 
-export function setupConfig<E extends { CONFIG_FILEPATH: string }, F>(
-  envSchema: ZodSchema<E>,
-  fileSchema: ZodSchema<F>,
-): { env: E; config: F };
-export function setupConfig<E, F>(envSchema: ZodSchema<E>, fileSchema: ZodSchema<F>): { env: E };
-export function setupConfig<E extends { CONFIG_FILEPATH?: string }, F>(
-  envSchema: ZodSchema<E>,
-  fileSchema: ZodSchema<F>,
-): { env: E; config?: F } {
-  let env: z.infer<ZodType<E>>;
+export async function setupConfig<E extends { CONFIG_FILEPATH: string }, F>(
+  envSchema: StandardSchemaV1<E>,
+  fileSchema: StandardSchemaV1<F>,
+): Promise<{ env: E; config: F }>;
+export async function setupConfig<E, F>(
+  envSchema: StandardSchemaV1<E>,
+  fileSchema: StandardSchemaV1<F>,
+): Promise<{ env: E }>;
+export async function setupConfig<E extends { CONFIG_FILEPATH?: string }, F>(
+  envSchema: StandardSchemaV1<E>,
+  fileSchema: StandardSchemaV1<F>,
+): Promise<{ env: E; config?: F }> {
+  let env = envSchema['~standard'].validate(process.env);
+  if (env instanceof Promise) env = await env;
 
-  try {
-    env = envSchema.parse(process.env);
-  } catch (error) {
-    logger.error('Failed to parse configuration from environment', { error });
-    throw error;
+  if (env.issues) {
+    logger.error('Failed to parse configuration from environment', { issues: env.issues });
+    throw new Error(JSON.stringify(env.issues, null, 2));
   }
 
-  if (!env.CONFIG_FILEPATH) {
+  if (!env.value.CONFIG_FILEPATH) {
     return {
-      env,
+      env: env.value,
     };
   }
 
-  try {
-    const content = readFileSync(env.CONFIG_FILEPATH, 'utf-8');
-    const config = fileSchema.parse(JSON.parse(content));
+  const content = readFileSync(env.value.CONFIG_FILEPATH, 'utf-8');
+  let config = fileSchema['~standard'].validate(JSON.parse(content));
+  if (config instanceof Promise) config = await config;
 
-    return {
-      env,
-      config,
-    };
-  } catch (error) {
-    logger.error('Failed to parse configuration from file', { error });
-    throw error;
+  if (config.issues) {
+    logger.error('Failed to parse configuration from file', { issues: config.issues });
+    throw new Error(JSON.stringify(config.issues, null, 2));
   }
+
+  return {
+    env: env.value,
+    config: config.value,
+  };
 }
