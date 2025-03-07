ab144f4ffd15d5fc213a0ceba573371fcd28b53a
Marian Eickmeyer <mail@marianheinsen.com>
2025-03-07 20:54:52 +0100

    feat: add stripe module

---
+++ stripe/index.ts
@@ -0,0 +1,24 @@
+import type Stripe from 'stripe';
+
+/**
+ * Auto pagination of Stripe list resources.
+ * Stripe's Node.js SDK already has auto pagination but
+ * with an upper limit of objects for performance reasons.
+ * @param listFn Callback to list Stripe objects.
+ * @param startingAfter Token to fetch objects starting after the given ID.
+ * @returns Array of Stripe objects.
+ */
+export async function getPagedStripeObject<T extends { id: string }>(
+  listFn: (params: Stripe.PaginationParams) => Stripe.ApiListPromise<T>,
+  startingAfter?: string,
+): Promise<T[]> {
+  const { data, has_more } = await listFn({
+    starting_after: startingAfter,
+  });
+
+  if (has_more) {
+    return [...data, ...(await getPagedStripeObject(listFn, data[data.length - 1]?.id))];
+  }
+
+  return data;
+}
