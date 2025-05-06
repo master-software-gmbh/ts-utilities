7cbfcf0487eca93fad1f3976b016ab5ca58ceedc
Marian Eickmeyer <mail@marianheinsen.com>
2025-05-06 18:41:43 +0200

    fix: export consent module

---
+++ consent/index.ts
@@ -0,0 +1,7 @@
+export * from './application/ConsentRepositoryImpl';
+export * from './domain/ConsentRepository';
+export * from './domain/ConsentService';
+export * from './domain/model/Consent';
+export * from './domain/model/ConsentStatus';
+export * from './infrastructure/migrations';
+export type { DB } from './infrastructure/types.d';
