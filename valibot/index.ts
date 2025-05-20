52a502ab46c8fb4c41bdc1e6ed08e879582be443
Marian Eickmeyer <mail@marianheinsen.com>
2025-05-20 09:26:18 +0200

    feat: add valibot helper

--- valibot/index.ts
+++ valibot/index.ts
@@ -1,4 +1,4 @@
-import { array, optional, pipe, string, transform, union, type GenericSchema } from 'valibot';
+import { type GenericSchema, array, fallback, optional, pipe, string, transform, union } from 'valibot';
 
 export function omitEmptyFile() {
   return transform<File, File | undefined>((file) => (file.size > 0 ? file : undefined));
@@ -12,6 +12,23 @@ export function htmlCheckbox() {
   return pipe(
     optional(string(), 'off'),
     transform<string, boolean>((value) => value === 'on'),
+  );
+}
+
+export function booleanString(defaultValue?: boolean) {
+  if (defaultValue === undefined) {
+    return pipe(
+      string(),
+      transform<string, boolean>((value) => value === 'true'),
+    );
+  }
+
+  return fallback(
+    pipe(
+      string(),
+      transform<string, boolean>((value) => value === 'true'),
+    ),
+    defaultValue,
   );
 }
 
