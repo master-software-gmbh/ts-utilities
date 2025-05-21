e2bca83b50de802ffaed4da1ec09bee7a4189b82
Marian Eickmeyer <mail@marianheinsen.com>
2025-05-21 15:21:48 +0200

    feat: add distance helper for sqlite

--- kysely/sqlite.ts
+++ kysely/sqlite.ts
@@ -1,4 +1,4 @@
-import { CompiledQuery } from 'kysely';
+import { CompiledQuery, sql, type Expression } from 'kysely';
 
 /**
  * Returns a query that enables foreign key constraints.
@@ -23,4 +23,28 @@ export function enableWalMode(schema?: string): CompiledQuery {
  */
 export function listDatabases(): CompiledQuery {
   return CompiledQuery.raw('PRAGMA database_list;');
+}
+
+/**
+ * Returns an expression that calculates the Haversine distance between two points.
+ * @param lat latitude of the reference point
+ * @param lng longitude of the reference point
+ * @param latColumn name of the latitude column
+ * @param lngColumn name of the longitude column
+ */
+export function haversineDistance(
+  lat: number,
+  lng: number,
+  latColumn: Expression<number | null>,
+  lngColumn: Expression<number | null>,
+) {
+  return sql<number>`
+    6371 * acos(
+      cos(radians(${lat}))
+      * cos(radians(${latColumn}))
+      * cos(radians(${lngColumn}) - radians(${lng}))
+      + sin(radians(${lat}))
+      * sin(radians(${latColumn}))
+    )
+  `;
 }
