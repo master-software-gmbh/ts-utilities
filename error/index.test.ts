4f5e7630d4b7bdd9391376bc5be844516d14018e
Marian Eickmeyer <mail@marianheinsen.com>
2025-06-30 11:36:44 +0200

    feat: update error message extraction

---
+++ error/index.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'bun:test';
+import { getErrorMessage } from '.';
+
+describe('getErrorMessage', () => {
+  it('should return message from Error', () => {
+    const error = new Error('This is an error.');
+    expect(getErrorMessage(error)).toEqual('This is an error.');
+  });
+
+  it('should return message from string', () => {
+    const error = 'This is a string.';
+    expect(getErrorMessage(error)).toEqual('This is a string.');
+  });
+
+  it('should return message from number', () => {
+    const error = 5;
+    expect(getErrorMessage(error)).toEqual('5');
+  });
+
+  it('should return message from object', () => {
+    const error = { foo: 'bar', n: 5 };
+    expect(getErrorMessage(error)).toEqual('{"foo":"bar","n":5}');
+  });
+});
--- error/index.ts
+++ error/index.ts
@@ -3,6 +3,10 @@ export function getErrorMessage(error: unknown): string {
     return error.message;
   }
 
+  if (typeof error === 'string') {
+    return error;
+  }
+
   try {
     return JSON.stringify(error);
   } catch {
