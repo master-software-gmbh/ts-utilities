cfe0c1f6b48a4ad03c749ecdf68c6f01e1a1f373
Marian Eickmeyer <mail@marianheinsen.com>
2025-07-10 11:24:13 +0200

    feat: add composite migrator

--- kysely/bun-sqlite/index.ts
+++ kysely/bun-sqlite/index.ts
@@ -1,3 +1,4 @@
 export * from './dialect.ts';
 export * from './generate.ts';
 export * from './generator.ts';
+export * from './migrator.ts';
---
+++ kysely/bun-sqlite/migrator.ts
@@ -0,0 +1,54 @@
+import { Migrator, type Kysely } from 'kysely';
+import type { MigrationProvider } from '../migrations/types';
+import { error, success, type Result } from '../../result';
+import { CompositeMigrationProvider } from '../migrations/provider';
+import { getErrorMessage } from '../../error';
+import { loadModule } from '../../esm';
+import { BunSqliteGeneratorDialect } from './generator';
+import { writeFile } from 'node:fs/promises';
+
+export class CompositeMigrator {
+  private readonly database: Kysely<any>;
+  private readonly providers: MigrationProvider[];
+
+  constructor(database: Kysely<any>, providers: MigrationProvider[]) {
+    this.database = database;
+    this.providers = providers;
+  }
+
+  async migrate(): Promise<Result<void, 'migration_failed'>> {
+    const provider = new CompositeMigrationProvider(this.providers);
+    const migrator = new Migrator({
+      db: this.database,
+      provider: provider,
+      allowUnorderedMigrations: true,
+    });
+
+    const result = await migrator.migrateToLatest();
+
+    if (result.error) {
+      return error('migration_failed', getErrorMessage(result.error));
+    }
+
+    return success();
+  }
+
+  async generate(filepath: string): Promise<Result<void, 'missing_dependencies'>> {
+    const { data: module } = await loadModule<typeof import('kysely-codegen')>('kysely-codegen');
+
+    if (!module) {
+      return error('missing_dependencies');
+    }
+
+    const result = await module.generate({
+      db: this.database,
+      dialect: new BunSqliteGeneratorDialect(),
+    });
+
+    const content = result.replace(/"/g, "'");
+
+    await writeFile(filepath, content);
+
+    return success();
+  }
+}
