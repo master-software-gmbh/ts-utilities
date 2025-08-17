a19d30c0c13a0331523aeef3c00fb493a4abb191
Marian Eickmeyer <mail@marianheinsen.com>
2025-08-17 21:46:32 +0200

    feat: update elster testmerker

--- elster/types/ElsterTestmerker.ts
+++ elster/types/ElsterTestmerker.ts
@@ -1,3 +1,15 @@
 export const ElsterTestmerker = {
-  Standard: '700000004',
+  /**
+   * Testfall, der sofort nach dem Empfang im jeweiligen Rechenzentrum des Bundeslandes gelöscht wird. Es erfolgt keine weitere Verarbeitung. Dient zum Test der Leitungsverbindung.
+   * Dieser Testmerker ist auch zu verwenden, um die Rückmeldung zu Anhängen mit dem Verfahren `ElsterDatenabholung` zu testen.
+   */
+  TestLeitungsverbindung: '700000001',
+  /**
+   * Testfall, der sofort nach Eingang in dem ELSTER-Annahmeserver gelöscht wird. Es erfolgt keine weitere Verarbeitung. Dient dem externen Softwarehersteller zum Testen der Datenübertragung.
+   */
+  TestDatenuebertragung: '700000004',
+  /**
+   * Testfall. Mit diesem Testmerker ist es möglich eine Server-Testantwort mit mehreren gefüllten Tags <ElsterInfo> zuerhalten. Der versandte Steuerfall wird in dem ELSTER-Annahmeserver direkt ausgesteuert.
+   */
+  TestElsterInfo: '010000001',
 } as const;
