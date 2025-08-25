c50c7b6358f395673f37a2fbf1b515405ec7b8eb
Marian Eickmeyer <mail@marianheinsen.com>
2025-08-25 21:23:38 +0200

    feat: update XArray

--- array/XArray.test.ts
+++ array/XArray.test.ts
@@ -4,17 +4,17 @@ import { XArray } from './XArray';
 describe('XArray', () => {
   describe('slices', () => {
     it('should return an empty iterator for an empty array', () => {
-      const result = XArray.slices([], 2);
+      const result = new XArray([]).slices(2);
       expect([...result]).toEqual([]);
     });
 
     it('should return slices of the specified size', () => {
-      const result = XArray.slices([1, 2, 3, 4, 5], 2);
+      const result = new XArray([1, 2, 3, 4, 5]).slices(2);
       expect([...result]).toEqual([[1, 2], [3, 4], [5]]);
     });
 
     it('should return slices of the specified size with a larger slice size', () => {
-      const result = XArray.slices([1, 2, 3, 4, 5], 3);
+      const result = new XArray([1, 2, 3, 4, 5]).slices(3);
       expect([...result]).toEqual([
         [1, 2, 3],
         [4, 5],
@@ -22,8 +22,31 @@ describe('XArray', () => {
     });
 
     it('should return slices of the specified size with a slice size equal to the array length', () => {
-      const result = XArray.slices([1, 2, 3], 3);
+      const result = new XArray([1, 2, 3]).slices(3);
       expect([...result]).toEqual([[1, 2, 3]]);
+    });
+  });
+
+  describe('compactMap', () => {
+    it('should filter out null and undefined', () => {
+      const expected = [1, 2, 3, 4];
+      const result = new XArray([1, 2, 3, null, 4, undefined]).compactMap((e) => e);
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should filter out null and undefined with custom transform', () => {
+      const expected = [2, 4];
+      const result = new XArray([1, 2, 3, 4]).compactMap((e) => (e % 2 === 0 ? e : null));
+
+      expect(result).toEqual(expected);
+    });
+
+    it('should filter out null and undefined with custom transform', () => {
+      const expected = [5, 'test'];
+      const result = new XArray([{ value: null }, { value: 5 }, { value: 'test' }, { value: null }]).compactMap((e) => e.value);
+
+      expect(result).toEqual(expected);
     });
   });
 })
--- array/XArray.ts
+++ array/XArray.ts
@@ -1,19 +1,31 @@
-export class XArray<T> extends Array<T> {
+export class XArray<T> {
+  private readonly array: Array<T>;
+
+  constructor(array: Array<T>) {
+    this.array = array;
+  }
+
   /**
-   * Splits the array into slices of the specified size.
+   * Transforms the elements of the array using the provided transform function and filters out any `null` or `undefined` values from the result.
    */
-  static *slices<T>(array: T[], size: number): IterableIterator<T[]> {
-    for (let i = 0; i < array.length; i += size) {
-      yield array.slice(i, i + size);
-    }
+  compactMap<U = Exclude<T, null | undefined>>(transform: (element: T) => U | null | undefined): U[] {
+    return this.array.reduce<U[]>((acc, element) => {
+      const result = transform(element);
+  
+      if (result !== null && result !== undefined) {
+        acc.push(result);
+      }
+  
+      return acc;
+    }, []);
   }
 
   /**
    * Splits the array into slices of the specified size.
    */
   *slices (size: number): IterableIterator<T[]> {
-    for (let i = 0; i < this.length; i += size) {
-      yield this.slice(i, i + size);
+    for (let i = 0; i < this.array.length; i += size) {
+      yield this.array.slice(i, i + size);
     }
   }
 }
