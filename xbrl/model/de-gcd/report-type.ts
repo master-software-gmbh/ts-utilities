2ba51ac1281fe2a0621eaf474c2b924e7c2378b4
Marian Eickmeyer <mail@marianheinsen.com>
2025-10-05 21:01:12 +0200

    chore: update gcd report type

--- xbrl/model/de-gcd/report-type.ts
+++ xbrl/model/de-gcd/report-type.ts
@@ -1,3 +1,4 @@
 export const ReportType = {
   Jahresabschluss: 'de-gcd_genInfo.report.id.reportType.reportType.JA',
+  SonstigerBericht: 'de-gcd_genInfo.report.id.reportType.reportType.S',
 } as const;
